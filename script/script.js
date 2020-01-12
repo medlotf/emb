b09e5bebaefa96fcdf6eeee4e9c5140d84bd37e8
Mohamed LOTF <mohamed.lotf@outlook.com>
2020-01-12 22:37:37 +0100

    all files

---
+++ script/script.js
@@ -0,0 +1,17 @@
+$(function() {
+    var slickOpts = {
+        dots: true,
+        slidesToShow: 1,
+        centerMode:true,
+        arrows: false,
+        infinite: true,
+        speed: 500,
+        autoplay: true
+    };
+    $(".persons").slick(slickOpts);
+    var slickEnabled = true;
+});
+
+
+
+
