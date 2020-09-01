dd7c337824e3dcb3ae46114317d91a9c3d353e44
Mohamed LOTF <mohamed.lotf@outlook.com>
2020-09-01 23:56:50 +0200

    add all files

--- script/script.js
+++ script/script.js
@@ -1,17 +1,29 @@
-$(function() {
+$(function () {
     var slickOpts = {
         dots: true,
         slidesToShow: 1,
-        centerMode:true,
+        centerMode: true,
         arrows: false,
         infinite: true,
         speed: 500,
-        autoplay: true
+        autoplay: true,
     };
     $(".persons").slick(slickOpts);
     var slickEnabled = true;
 });
 
-
-
-
+$(document).ready(function () {
+    $("#testimonial-slider").owlCarousel({
+        items: 1,
+        itemsDesktop: [1000, 1],
+        itemsDesktopSmall: [979, 1],
+        itemsTablet: [768, 1],
+        pagination: true,
+        navigation: false,
+        navigationText: ["", ""],
+        slideSpeed: 1000,
+        singleItem: true,
+        transitionStyle: "fade",
+        autoPlay: true,
+    });
+});
